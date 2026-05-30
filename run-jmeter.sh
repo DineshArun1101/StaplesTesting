ce71cbffe3f4bfc6f41b23624abdcf8708359488
Dinesh A <dinesha01.official@gmail.com>
2026-05-30 22:03:36 +0530

    updated JMX and Sh file for min conversion

--- run-jmeter.sh
+++ run-jmeter.sh
@@ -4,8 +4,11 @@ JMETER_HOME=$1
 REPORT_NAME=$2
 
 THREADS_SAMPLE_ITF=10
-RAMPUP_ITF=1
+RAMPUP_ITF=60
 TESTDURATION_ITF=300
+
+RAMPUP_ITF_MIN=$((RAMPUP_ITF / 60))
+TESTDURATION_ITF_MIN=$((TESTDURATION_ITF / 60))
 
 # Result Folder
 RESULT_DIR="results-history/results_${REPORT_NAME}"
@@ -21,8 +24,11 @@ HTML_REPORT="$RESULT_DIR/html-report"
 echo "======================================="
 echo "Starting JMeter Load Test"
 echo "Threads_ITF        : $THREADS_SAMPLE_ITF"
-echo "Rampup_ITF         : $RAMPUP_ITF"
-echo "Duration_ITF       : $TESTDURATION_ITF"
+echo "Rampup_ITF_SEC         : $RAMPUP_ITF"
+echo "Duration_ITF_SEC       : $TESTDURATION_ITF"
+
+echo "Rampup_ITF_MIN         : $RAMPUP_ITF_MIN"
+echo "Duration_ITF_MIN       : $TESTDURATION_ITF_MIN"
 echo "Report Name    : $REPORT_NAME"
 echo "Result Folder  : $RESULT_DIR"
 echo "======================================="
@@ -38,7 +44,13 @@ echo "======================================="
   -Jtestduration_itf=$TESTDURATION_ITF \
   -Gthreads_sample_itf=$THREADS_SAMPLE_ITF \
   -Grampup_itf=$RAMPUP_ITF \
-  -Gtestduration_itf=$TESTDURATION_ITF
+  -Gtestduration_itf=$TESTDURATION_ITF \
+
+  -Jrampup_itf_min=$RAMPUP_ITF_MIN \
+  -Jtestduration_itf_min=$TESTDURATION_ITF_MIN \
+
+  -Grampup_itf_min=$RAMPUP_ITF_MIN \
+  -Gtestduration_itf_min=$TESTDURATION_ITF_MIN
 
 EXIT_CODE=$?
 
